e263c5773bff1f49f5f732f6eb158a852626af1b
syaeful16 <syaefulanas121@gmail.com>
2025-07-14 07:27:09 +0700

    add: create utils

---
+++ src/main/java/com/syan/dev/utils/Utils.java
@@ -0,0 +1,17 @@
+package com.syan.dev.utils;
+
+import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.model.FormDefinition;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.app.dao.FormDefinitionDao;
+
+public class Utils {
+    public String getFormJson(String formDefId) {
+        String formJson = null;
+
+        AppDefinition appDef = AppUtil.getCurrentAppDefinition();
+
+
+        return null;
+    }
+}
