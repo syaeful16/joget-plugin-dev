c0a17d486b682b53de74169ae4b6b8e3e01c37f6
syaeful16 <syaefulanas121@gmail.com>
2025-07-20 22:59:26 +0700

    create: form grid service

--- src/main/java/com/syan/dev/services/FormGridCustomService.java
+++ src/main/java/com/syan/dev/services/FormGridCustomService.java
@@ -1,21 +1,11 @@
 package com.syan.dev.services;
 
 import com.syan.dev.func.FormGridCustomFunc;
-import org.joget.apps.app.service.AppService;
-import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.model.FormData;
-import org.joget.commons.util.SetupManager;
 import org.joget.commons.util.StringUtil;
-import org.joget.workflow.util.WorkflowUtil;
 
-import javax.servlet.http.HttpServletRequest;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.Locale;
 import java.util.Map;
 
 public class FormGridCustomService {
