cdf82383c99d927c8377930c5625752b1e68ca45
syaeful16 <syaefulanas121@gmail.com>
2025-07-20 21:25:30 +0700

    create: create function escapeJson

--- src/main/java/com/syan/dev/utils/Utils.java
+++ src/main/java/com/syan/dev/utils/Utils.java
@@ -6,12 +6,13 @@ import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.app.dao.FormDefinitionDao;
 
 public class Utils {
-    public String getFormJson(String formDefId) {
-        String formJson = null;
-
-        AppDefinition appDef = AppUtil.getCurrentAppDefinition();
-
-
-        return null;
+    public String escapeJson(Object obj) {
+        if (obj == null) return "";
+        return obj.toString()
+                .replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
     }
 }
